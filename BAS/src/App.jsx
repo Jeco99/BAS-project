1603363f33bec22cf5e8c5ecce8c3c1b764d46b9
emmanuel tanierla jr <bonznia@yahoo.com>
2023-07-17 10:56:15 +0800

    updated task

--- BAS/src/App.jsx
+++ BAS/src/App.jsx
@@ -2,14 +2,17 @@ import LogIn from "./LogIn";
 import BrgyNavbar from "./components/barangay/barangayNavBar/BarangayNavBar";
 import BarangayDashboard from "./components/barangay/barangayDashboard/barangayDashboard";
 import AccountDetails from "./components/user/userSignUp/accountDetails";
+import BarangayAddPost from "./components/barangay/barangayDashboard/addpost";
+import BarangaySignUp from "./components/barangay/barangaySignup.jsx/BarangaySignUp";
 
 function App() {
   return (
     <>
-     <LogIn />      
+     {/* <LogIn />       */}
       {/* <BrgyNavbar/> */}
       {/* <BarangayDashboard /> */}
       {/* <AccountDetails /> */}
+      <BarangaySignUp />
     </>
   );
 }
--- BAS/src/LogIn.jsx
+++ BAS/src/LogIn.jsx
@@ -1,27 +1,37 @@
 import { Button, Label, TextInput } from "flowbite-react";
+import logoImage from "/src/assets/images/BAS-Logo-1.2.png";
 
 const LogIn = () => {
-  const img = "../images/BAS-Logo-1.png";
-  return (
-    <div className="Login flex">
-      <img className="Img" src={img} alt="" />
-      <form className="m-8 py-5 px-10 border-2 border-black rounded-3xl">
-        <div className="text-center font-extrabold">
-          <p>Log in</p>
-        </div>
-        <div className="py-5">
-            <Label htmlFor="email1" value="Email Address:" />
-          <TextInput id="email1" placeholder="" required type="email" />    
-            <Label htmlFor="password" value="Password" />
-          <TextInput id="password" required type="password" />
-          </div>
-        <button className="btn">
-          <Button type="button">Log In</Button>
-          <br />
-          <Button type="button">Create account</Button>
-        </button>
-      </form>
-    </div>
-  );
+ return (
+   <div className="">
+     <div className="Login flex justify-between items-center px-10">
+       <img className="Img m-5" src={logoImage} alt="" />
+       <form className="m-8 py-5 px-10 border-2 border-black rounded-3xl w-80 h-96">
+         <div className="text-center font-extrabold">
+           <p>Log in</p>
+         </div>
+         <div className="py-5">
+           <Label htmlFor="email1" value=" Email Address:" />
+           <TextInput id="email1" placeholder="" required type="email" />
+           <Label htmlFor="password" value="Password" />
+           <TextInput id="password" required type="password" />
+         </div>
+         <Button
+           type="button"
+           className="rounded-full bg-teal-300 w-44 h-11 text-black"
+         >
+           Log In
+         </Button>
+         <br />
+         <Button
+           type="button"
+           className="rounded-full bg-teal-300 w-44 h-11 text-black"
+         >
+           Create Account
+         </Button>
+       </form>
+     </div>
+   </div>
+ );
 };
 export default LogIn;
