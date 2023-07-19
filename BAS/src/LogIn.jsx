0ff1c9c06f8be1d418bb9e62f4e8b7f46430aba0
Jeco99 <jerriccojohncabunagan00@gmail.com>
2023-07-19 21:13:57 +0800

    add router

--- BAS/src/LogIn.jsx
+++ BAS/src/LogIn.jsx
@@ -1,19 +1,22 @@
-import logoImage from "/src/assets/images/BAS-Logo-1.2.png";
+import { Link } from "react-router-dom";
+import logoImage from "/src/assets/images/BAS-Logo-1.png";
 
 const LogIn = () => {
   return (
-    <div className="bg-beetleGreen flex justify-between items-center flex-col sm:flex-row px-10 py-16">
+    <div className="bg-beetleGreen flex justify-between items-center  h-screen px-10 py-16">
       <img className="w-5/12 h-auto py-6" src={logoImage} alt="" />
-      <form className="bg-beetleGreen block m-8 p-8 sm:m-32 border border-black rounded-lg shadow">
-        <h1 className="text-center text-5xl font-extrabold m-2">Log in</h1>
-        <div className="mb-2 block">
+      <form className="bg-beetleGreen block m-4 p-8 border border-black rounded-lg">
+        <h1 className="text-center text-3xl sm:text-5xl font-extrabold mb-6">
+          Log in
+        </h1>
+        <div className="mb-4">
           <div>
-            <label htmlFor="email1" value=" Email Address:" className="text-xl">
-              Email Address:
+            <label htmlFor="email1" className="text-xl">
+              Username:
             </label>
           </div>
           <input
-            className="w-full px-20 text-black-500 bg-white border rounded-md shadow-sm outline-none appearance-none focus:border-indigo-600"
+            className="w-full px-4 py-2 text-black bg-white border rounded-md shadow-sm outline-none appearance-none focus:border-indigo-600"
             id="email1"
             placeholder=""
             required
@@ -21,34 +24,35 @@ const LogIn = () => {
           />
         </div>
 
-        <div className="mb-2 block">
+        <div className="mb-4">
           <div>
-            <label htmlFor="password" value="Password" className="text-xl">
+            <label htmlFor="password" className="text-xl">
               Password:
             </label>
           </div>
           <input
-            className="w-full px-20 text-black-500 bg-white border rounded-md shadow-sm outline-none appearance-none focus:border-indigo-600"
+            className="w-full px-4 py-2 text-black bg-white border rounded-md shadow-sm outline-none appearance-none focus:border-indigo-600"
             id="password"
             required
             type="password"
           />
         </div>
 
-        <div className="flex flex-col justify-center">
-          <button
+        <div className="flex flex-col  justify-center">
+          <Link 
+            to="/userdashboard"
             type="button"
-            className="rounded-full text-xl h-11 text-black bg-morningGlory"
+            className="rounded-full text-xl h-11 text-white bg-morningGlory w-full sm:w-auto mb-4 sm:mb-2"
           >
             Log In
-          </button>
-          <br />
-          <button
+          </Link>
+          <Link
+            to="/createaccount"
             type="button"
-            className="rounded-full text-xl h-11 text-black bg-morningGlory"
+            className="rounded-full text-xl h-11 text-white bg-morningGlory w-full sm:w-auto"
           >
             Create Account
-          </button>
+          </Link>
         </div>
       </form>
     </div>
