77d76ef2cdaa77e2710cc0ed1925a379606defa7
Jeco99 <jerriccojohncabunagan00@gmail.com>
2023-07-26 20:44:53 +0800

    added data validation and fix next button in account

--- BAS/src/components/user/userSignUp/accountDetails.jsx
+++ BAS/src/components/user/userSignUp/accountDetails.jsx
@@ -3,105 +3,113 @@ import ImageUpload from "./imageUpload";
 
 import { Button } from "@material-tailwind/react";
 
-
-function AccountDetails({getData, setGetData, handleNext, handlePrev, activeStep, isFirstStep}) {
- 
+function AccountDetails({
+  getData,
+  setGetData,
+  handleNext,
+  handlePrev,
+  activeStep,
+  isFirstStep,
+  errors,
+}) {
   const handleChange = (e) => {
-      e.preventDefault()    
-        const { name, value } = e.target;
-        setGetData({
-          ...getData,
-          [name]: value,
-        });   
-  }
+    e.preventDefault();
+    const { name, value } = e.target;
+    setGetData({
+      ...getData,
+      [name]: value,
+    });
+  };
 
   return (
     <div>
-      <h1 className="text-center text-2xl sm:text-4xl py-4 font-semibold">Account Details</h1>
+      <h1 className="text-center text-2xl sm:text-4xl py-4 font-semibold">
+        Account Details
+      </h1>
       <div className="block mt-8 p-6  bg-white border border-gray-200 rounded-lg shadow">
-        
-        <form className="w-full" >
-        <div className="flex flex-col gap-4 ">
-          <ImageUpload getData={getData} setGetData={setGetData} />
-          <div>
-            <div className="mb-2 block">
-              <label htmlFor="userName">
-                Username
-              </label>
+        <div className="w-full">
+          <div className="flex flex-col gap-4 ">
+            <ImageUpload getData={getData} setGetData={setGetData} />
+            <div>
+              <div className="mb-2 block">
+                <label htmlFor="userName">Username</label>
+              </div>
+              <input
+                className="w-full p-2.5 text-black-500 bg-white border rounded-md shadow-sm outline-none appearance-none focus:border-indigo-600"
+                id="username"
+                name="username"
+                required
+                type="text"
+                onChange={handleChange}
+                value={getData.username}
+              />
+              {errors.username && <small>User Name is required!</small>}
             </div>
-            <input
-              className="w-full p-2.5 text-black-500 bg-white border rounded-md shadow-sm outline-none appearance-none focus:border-indigo-600"
-              id="username"
-              name="username"
-              required
-              type="text"
-              onChange={handleChange}
-            />
-          </div>
-          <div>
-            <div className="mb-2 block">
-              <label htmlFor="email" value="Your email">
-                Email
-              </label>
+            <div>
+              <div className="mb-2 block">
+                <label htmlFor="email" value="Your email">
+                  Email
+                </label>
+              </div>
+              <input
+                className="w-full p-2.5 text-black-500 bg-white border rounded-md shadow-sm outline-none appearance-none focus:border-indigo-600"
+                id="email"
+                name="email"
+                required
+                type="email"
+                onChange={handleChange}
+                value={getData.email}
+              />
+              {errors.email && <small>Email is required!</small>}
             </div>
-            <input
-              className="w-full p-2.5 text-black-500 bg-white border rounded-md shadow-sm outline-none appearance-none focus:border-indigo-600"
-              id="email"
-              name="email"
-              required
-              type="email"
-              onChange={handleChange}
-            />
-          </div>
-          <div>
-            <div className="mb-2 block">
-              <label htmlFor="password" value="Your password">
-                Password
-              </label>
+            <div>
+              <div className="mb-2 block">
+                <label htmlFor="password" value="Your password">
+                  Password
+                </label>
+              </div>
+              <input
+                className="w-full p-2.5 text-black-500 bg-white border rounded-md shadow-sm outline-none appearance-none focus:border-indigo-600"
+                id="password"
+                name="password"
+                required
+                type="password"
+                onChange={handleChange}
+                value={getData.password}
+              />
+              {errors.password && <small>Password is required!</small>}
             </div>
-            <input
-              className="w-full p-2.5 text-black-500 bg-white border rounded-md shadow-sm outline-none appearance-none focus:border-indigo-600"
-              id="password"
-              name="password"
-              required
-              type="password"
-              onChange={handleChange}
-            />
-          </div>
-          <div>
-            <div className="mb-2 block">
-              <label htmlFor="confirmpassword" value="Confirm password">
-                Confirm Password
-              </label>
+            <div>
+              <div className="mb-2 block">
+                <label htmlFor="confirmpassword" value="Confirm password">
+                  Confirm Password
+                </label>
+              </div>
+              <input
+                className="w-full p-2.5 text-black-500 bg-white border rounded-md shadow-sm outline-none appearance-none focus:border-indigo-600"
+                id="confirmpassword"
+                name="confirmpassword"
+                required
+                type="password"
+                onChange={handleChange}
+                value={getData.confirmpassword}
+              />
+              {errors.confirmpassword && <small>Password is required!</small>}
             </div>
-            <input
-              className="w-full p-2.5 text-black-500 bg-white border rounded-md shadow-sm outline-none appearance-none focus:border-indigo-600"
-              id="confirmpassword"
-              name="confirmpassword"
-              required
-              type="password"
-              onChange={handleChange}
-            />
           </div>
-          </div>
-                 
+
           <div className="mt-16 flex justify-between">
-        <Button
-          onClick={handlePrev}
-          disabled={isFirstStep}          
-        >
-          Prev
-        </Button>
-        <Button
-          onClick={handleNext}
-          type= {activeStep == 0 ? "button" : "submit"}
-        >
-          {activeStep == 0 ? "Next" : "Submit"}
-        </Button>
-      </div>
-          </form>
-  
-  
+            <Button onClick={handlePrev} disabled={isFirstStep}>
+              Prev
+            </Button>
+            <Button
+              onClick={handleNext}
+              type={activeStep == 0 ? "button" : "submit"}
+            >
+              {activeStep == 0 ? "Next" : "Submit"}
+            </Button>
+          </div>
+        </div>
       </div>
     </div>
   );
--- BAS/src/components/user/userSignUp/personalDetails.jsx
+++ BAS/src/components/user/userSignUp/personalDetails.jsx
@@ -10,9 +10,14 @@ import ProvinceDropdown from "./provinceDropdown";
 
 import { Button } from "@material-tailwind/react";
 
-
-function PersonalDetails({ getData, setGetData, handleNext, handlePrev, activeStep, isFirstStep }) {
-
+function PersonalDetails({
+  getData,
+  setGetData,
+  handleNext,
+  handlePrev,
+  activeStep,
+  isFirstStep,
+}) {
   const handleChange = (e) => {
     e.preventDefault();
     const { name, value } = e.target;
@@ -23,112 +28,124 @@ function PersonalDetails({ getData, setGetData, handleNext, handlePrev, activeSt
   };
 
   const [errors, setErrors] = useState({
-    firstname:'',
-    middlename:'',
-    lastname:'',
-    sex:'',
-    dateofbirth:'',
-    civilstatus:'',
-    barangay:'',
-    municipal:'',
-    province:'',
-    zone:'',
-    street:'',
-    zipcode:''
-  })
-
+    firstname: null,
+    middlename: null,
+    lastname: null,
+    sex: null,
+    dateofbirth: null,
+    civilstatus: null,
+    barangay: null,
+    municipal: null,
+    province: null,
+    zone: null,
+    street: null,
+    zipcode: null,
+  });
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    
-    let newErrors = {...errors};
 
-    if(getData.firstname.trim() == ''){
-      newErrors.firstname = 'Set FirstName'
-     
+    let newErrors = { ...errors };
+
+    if (getData.firstname.trim() == "") {
+      newErrors.firstname = "Set FirstName";
     } else {
-      newErrors.firstname = ''
+      newErrors.firstname = "";
     }
 
-    if(getData.middlename.trim() == ''){
-      newErrors.middlename = 'Set MiddleName'
+    if (getData.middlename.trim() == "") {
+      newErrors.middlename = "Set MiddleName";
     } else {
-      newErrors.middlename = ''
+      newErrors.middlename = "";
     }
 
-    if(getData.lastname.trim() == ''){
-      newErrors.lastname = 'Set LastName'
+    if (getData.lastname.trim() == "") {
+      newErrors.lastname = "Set LastName";
     } else {
-      newErrors.lastname = ''
+      newErrors.lastname = "";
     }
 
-    if(getData.sex.trim() == ''){
-      newErrors.sex = 'Set Sex'
+    if (getData.sex.trim() == "") {
+      newErrors.sex = "Set Sex";
     } else {
-      newErrors.sex = ''
+      newErrors.sex = "";
     }
 
-    if(getData.dateofbirth.trim() == ''){
-      newErrors.dateofbirth = 'Set Birthday'
+    if (getData.dateofbirth.trim() == "") {
+      newErrors.dateofbirth = "Set Birthday";
     } else {
-      newErrors.dateofbirth = ''
+      newErrors.dateofbirth = "";
     }
 
-    if(getData.civilstatus.trim() == ''){
-      newErrors.civilstatus = 'Set Civil Status'
+    if (getData.civilstatus.trim() == "") {
+      newErrors.civilstatus = "Set Civil Status";
     } else {
-      newErrors.civilstatus = ''
+      newErrors.civilstatus = "";
     }
 
-    if(getData.barangay.trim() == ''){
-      newErrors.barangay = 'Set Barangay'
+    if (getData.barangay.trim() == "") {
+      newErrors.barangay = "Set Barangay";
     } else {
-      newErrors.barangay = ''
+      newErrors.barangay = "";
     }
 
-    if(getData.municipal.trim() == ''){
-      newErrors.municipal = 'Set Municipal'
+    if (getData.municipal.trim() == "") {
+      newErrors.municipal = "Set Municipal";
     } else {
-      newErrors.municipal = ''
+      newErrors.municipal = "";
     }
 
-    if(getData.municipal.trim() == ''){
-      newErrors.municipal = 'Set Municipal'
+    if (getData.municipal.trim() == "") {
+      newErrors.municipal = "Set Municipal";
     } else {
-      newErrors.municipal = ''
+      newErrors.municipal = "";
     }
 
-    if(getData.province.trim() == ''){
-      newErrors.province = 'Set Province'
+    if (getData.province.trim() == "") {
+      newErrors.province = "Set Province";
     } else {
-      newErrors.province = ''
+      newErrors.province = "";
     }
 
-    if(getData.zone.trim() == ''){
-      newErrors.zone = 'Set Zone'
+    if (getData.zone.trim() == "") {
+      newErrors.zone = "Set Zone";
     } else {
-      newErrors.zone = ''
+      newErrors.zone = "";
     }
 
-    if(getData.street.trim() == ''){
-      newErrors.street = 'Set Street'
+    if (getData.street.trim() == "") {
+      newErrors.street = "Set Street";
     } else {
-      newErrors.street = ''
+      newErrors.street = "";
     }
 
-    if(getData.zipcode.trim() == ''){
-      newErrors.zipcode = 'Set Zipcode'
+    if (getData.zipcode.trim() == "") {
+      newErrors.zipcode = "Set Zipcode";
     } else {
-      newErrors.zipcode = ''
+      newErrors.zipcode = "";
     }
 
     setErrors(newErrors);
 
-    alert('Form Submitted!');
-  }
+    if(errors.firstname === '' && 
+      errors.middlename === '' &&
+      errors.lastname === '' &&
+      errors.sex === '' &&
+      errors.dateofbirth === '' &&
+      errors.civilstatus === '' &&
+      errors.barangay === '' &&
+      errors.municipal === '' &&
+      errors.province === '' &&
+      errors.zone === '' &&
+      errors.street === '' &&
+      errors.zipcode === ''
+    ){
+      alert('Form Submitted');
+      //redirect to user dashboard
+    }
+  };
 
-
-  
+  console.log(errors)
 
   return (
     <div>
@@ -136,179 +153,203 @@ function PersonalDetails({ getData, setGetData, handleNext, handlePrev, activeSt
         Personal Details
       </h1>
       <div className="block mt-8 p-6  bg-white border border-gray-200 rounded-lg shadow">
-     
         <form className="w-full" onSubmit={handleSubmit}>
-        <div className="grid grid-cols-1 gap-3 sm:grid-cols-2">
-          <div>
-            <div className="mb-2 block">
-              <label htmlFor="firstName">First Name <sup>*</sup></label>
+          <div className="grid grid-cols-1 gap-3 sm:grid-cols-2">
+            <div>
+              <div className="mb-2 block">
+                <label htmlFor="firstName">
+                  First Name <sup>*</sup>
+                </label>
+              </div>
+              <input
+                className="w-full p-2.5 text-black-500 bg-white border rounded-md shadow-sm outline-none appearance-none focus:border-indigo-600"
+                id="firstname"
+                name="firstname"
+                type="text"
+                onChange={handleChange}
+                value={getData.firstname}
+              />
+              {errors.firstname && <small>First Name is required!</small>}
             </div>
-            <input
-              className="w-full p-2.5 text-black-500 bg-white border rounded-md shadow-sm outline-none appearance-none focus:border-indigo-600"
-              id="firstname"
-              name="firstname"
-              type="text"
-              onChange={handleChange}
-            />
-            {errors.firstname && (<small>First Name is required!</small>)}
-          </div>
 
-          <div>
-            <div className="mb-2 block">
-              <label htmlFor="middleName">Middle Name<sup>*</sup></label>
+            <div>
+              <div className="mb-2 block">
+                <label htmlFor="middleName">
+                  Middle Name<sup>*</sup>
+                </label>
+              </div>
+              <input
+                className="w-full p-2.5 text-black-500 bg-white border rounded-md shadow-sm outline-none appearance-none focus:border-indigo-600"
+                id="middlename"
+                name="middlename"
+                type="text"
+                onChange={handleChange}
+                value={getData.middlename}
+              />
+              {errors.middlename && <small>Middle Name is required!</small>}
             </div>
-            <input
-              className="w-full p-2.5 text-black-500 bg-white border rounded-md shadow-sm outline-none appearance-none focus:border-indigo-600"
-              id="middlename"
-              name="middlename"
-              type="text"
-              onChange={handleChange}
-            />
-            {errors.middlename && (<small>Middle Name is required!</small>)}
-          </div>
 
-          <div>
-            <div className="mb-2 block">
-              <label htmlFor="lastName">Last Name<sup>*</sup></label>
+            <div>
+              <div className="mb-2 block">
+                <label htmlFor="lastName">
+                  Last Name<sup>*</sup>
+                </label>
+              </div>
+              <input
+                className="w-full p-2.5 text-black-500 bg-white border rounded-md shadow-sm outline-none appearance-none focus:border-indigo-600"
+                id="lastname"
+                name="lastname"
+                type="text"
+                onChange={handleChange}
+                value={getData.lastname}
+              />
+              {errors.lastname && <small>Last Name is required!</small>}
             </div>
-            <input
-              className="w-full p-2.5 text-black-500 bg-white border rounded-md shadow-sm outline-none appearance-none focus:border-indigo-600"
-              id="lastname"
-              name="lastname"
-              type="text"
-              onChange={handleChange}
-            />
-             {errors.lastname && (<small>Last Name is required!</small>)}
-          </div>
 
-          <div>
-            <div className="mb-2 block">
-              <label htmlFor="suffix">Suffix (optional)</label>
+            <div>
+              <div className="mb-2 block">
+                <label htmlFor="suffix">Suffix (optional)</label>
+              </div>
+              <input
+                className="w-full p-2.5 text-black-500 bg-white border rounded-md shadow-sm outline-none appearance-none focus:border-indigo-600"
+                id="suffix"
+                name="suffix"
+                type="text"
+                onChange={handleChange}
+                value={getData.suffix}
+              />
             </div>
-            <input
-              className="w-full p-2.5 text-black-500 bg-white border rounded-md shadow-sm outline-none appearance-none focus:border-indigo-600"
-              id="suffix"
-              name="suffix"
-              type="text"
-              onChange={handleChange}
-            />
-          </div>
 
-          <div className="mb-2">
-            <div className="mb-2 block ">
-              <label htmlFor="sex">
-                Sex <sup>*</sup>
-              </label>
+            <div className="mb-2">
+              <div className="mb-2 block ">
+                <label htmlFor="sex">
+                  Sex <sup>*</sup>
+                </label>
+              </div>
+              <GenderDropdown getData={getData} setGetData={setGetData} />
+              {errors.sex && <small>Sex is required!</small>}
             </div>
-            <GenderDropdown getData={getData} setGetData={setGetData} />
-            {errors.sex && (<small>Sex is required!</small>)}
-          </div>
 
-          <div>
-            <div className="mb-2 block">
-              <label htmlFor="dateofbirth">Date Of Birth<sup>*</sup></label>
+            <div>
+              <div className="mb-2 block">
+                <label htmlFor="dateofbirth">
+                  Date Of Birth<sup>*</sup>
+                </label>
+              </div>
+              <input
+                type="date"
+                name="dateofbirth"
+                id="dateofbirth"
+                className="w-full p-2.5 text-black-500 bg-white border rounded-md shadow-sm outline-none appearance-none focus:border-indigo-600"
+                onChange={handleChange}
+                value={getData.dateofbirth}
+              />
+              {errors.dateofbirth && <small>Birthday is required!</small>}
             </div>
-            <input
-              type="date"
-              name="dateofbirth"
-              id="dateofbirth"
-              className="w-full p-2.5 text-black-500 bg-white border rounded-md shadow-sm outline-none appearance-none focus:border-indigo-600"
-              onChange={handleChange}
-            />
-             {errors.dateofbirth && (<small>Birthday is required!</small>)}
-          </div>
 
-          <div className="mb-2">
-            <div className="mb-2 block">
-              <label htmlFor="civilStatus">Civil Status<sup>*</sup></label>
+            <div className="mb-2">
+              <div className="mb-2 block">
+                <label htmlFor="civilStatus">
+                  Civil Status<sup>*</sup>
+                </label>
+              </div>
+              <CivilStatusDropdown getData={getData} setGetData={setGetData} />
+              {errors.civilstatus && <small>Civil Status is required!</small>}
             </div>
-            <CivilStatusDropdown getData={getData} setGetData={setGetData} />
-            {errors.civilstatus && (<small>Civil Status is required!</small>)}
-          </div>
 
-          <div className="mb-2">
-            <div className="mb-2 block ">
-              <label htmlFor="barangay">Barangay<sup>*</sup></label>
+            <div className="mb-2">
+              <div className="mb-2 block ">
+                <label htmlFor="barangay">
+                  Barangay<sup>*</sup>
+                </label>
+              </div>
+              <BarangayDropdown getData={getData} setGetData={setGetData} />
+              {errors.barangay && <small>Barangay is required!</small>}
             </div>
-            <BarangayDropdown getData={getData} setGetData={setGetData} />
-            {errors.barangay && (<small>Barangay is required!</small>)}
-          </div>
 
-          <div className="mb-2">
-            <div className="mb-2 block ">
-              <label htmlFor="municipal">Municipality<sup>*</sup></label>
+            <div className="mb-2">
+              <div className="mb-2 block ">
+                <label htmlFor="municipal">
+                  Municipality<sup>*</sup>
+                </label>
+              </div>
+              <MunicipalDropdown getData={getData} setGetData={setGetData} />
+              {errors.municipal && <small>Municipal is required!</small>}
             </div>
-            <MunicipalDropdown getData={getData} setGetData={setGetData} />
-            {errors.municipal && (<small>Municipal is required!</small>)}
-          </div>
 
-          <div className="mb-2">
-            <div className="mb-2 block ">
-              <label htmlFor="province">Province<sup>*</sup></label>
+            <div className="mb-2">
+              <div className="mb-2 block ">
+                <label htmlFor="province">
+                  Province<sup>*</sup>
+                </label>
+              </div>
+              <ProvinceDropdown getData={getData} setGetData={setGetData} />
+              {errors.province && <small>Province is required!</small>}
             </div>
-            <ProvinceDropdown getData={getData} setGetData={setGetData} />
-            {errors.province && (<small>Province is required!</small>)}
-          </div>
 
-          <div>
-            <div className="mb-2 block">
-              <label htmlFor="zone">Zone<sup>*</sup></label>
+            <div>
+              <div className="mb-2 block">
+                <label htmlFor="zone">
+                  Zone<sup>*</sup>
+                </label>
+              </div>
+              <input
+                className="w-full p-2.5 text-black-500 bg-white border rounded-md shadow-sm outline-none appearance-none focus:border-indigo-600"
+                id="zone"
+                name="zone"
+                type="text"
+                onChange={handleChange}
+                value={getData.zone}
+              />
+              {errors.zone && <small>Zone is required!</small>}
             </div>
-            <input
-              className="w-full p-2.5 text-black-500 bg-white border rounded-md shadow-sm outline-none appearance-none focus:border-indigo-600"
-              id="zone"
-              name="zone"
-              type="text"
-              onChange={handleChange}
-            />
-             {errors.zone && (<small>Zone is required!</small>)}
-          </div>
 
-          <div>
-            <div className="mb-2 block">
-              <label htmlFor="street">Street<sup>*</sup></label>
+            <div>
+              <div className="mb-2 block">
+                <label htmlFor="street">
+                  Street<sup>*</sup>
+                </label>
+              </div>
+              <input
+                className="w-full p-2.5 text-black-500 bg-white border rounded-md shadow-sm outline-none appearance-none focus:border-indigo-600"
+                id="street"
+                name="street"
+                type="text"
+                onChange={handleChange}
+                value={getData.street}
+              />
+              {errors.street && <small>Street is required!</small>}
             </div>
-            <input
-              className="w-full p-2.5 text-black-500 bg-white border rounded-md shadow-sm outline-none appearance-none focus:border-indigo-600"
-              id="street"
-              name="street"
-              type="text"
-              onChange={handleChange}
-            />
-             {errors.street && (<small>Street is required!</small>)}
-          </div>
 
-          <div>
-            <div className="mb-2 block">
-              <label htmlFor="zipCode">Zip Code<sup>*</sup></label>
+            <div>
+              <div className="mb-2 block">
+                <label htmlFor="zipCode">
+                  Zip Code<sup>*</sup>
+                </label>
+              </div>
+              <input
+                className="w-full p-2.5 text-black-500 bg-white border rounded-md shadow-sm outline-none appearance-none focus:border-indigo-600"
+                id="zipcode"
+                name="zipcode"
+                type="text"
+                onChange={handleChange}
+                value={getData.zipcode}
+              />
+              {errors.zipcode && <small>Zip Code is required!</small>}
             </div>
-            <input
-              className="w-full p-2.5 text-black-500 bg-white border rounded-md shadow-sm outline-none appearance-none focus:border-indigo-600"
-              id="zipcode"
-              name="zipcode"
-              type="text"
-              onChange={handleChange}
-            />
-             {errors.zipcode && (<small>Zip Code is required!</small>)}
-          </div>
           </div>
           <div className="mt-16 flex justify-between">
-        <Button
-          onClick={handlePrev}
-          disabled={isFirstStep}          
-        >
-          Prev
-        </Button>
-        <Button
-          onClick={handleNext}
-          type= {activeStep == 0 ? "button" : "submit"}
-        >
-          {activeStep == 0 ? "Next" : "Submit"}
-        </Button>
-      </div>
+            <Button onClick={handlePrev} disabled={isFirstStep}>
+              Prev
+            </Button>
+            <Button
+              onClick={handleNext}
+              type={activeStep == 0 ? "button" : "submit"}
+            >
+              {activeStep == 0 ? "Next" : "Submit"}
+            </Button>
+          </div>
         </form>
-       
-      
       </div>
     </div>
   );
