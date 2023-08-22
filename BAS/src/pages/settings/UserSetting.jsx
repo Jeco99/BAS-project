d64ddd0f92a39f2a39ba53f0bf8759de83e36509
Jeco99 <jerriccojohncabunagan00@gmail.com>
2023-08-22 15:39:30 +0800

    update setting form account both user

--- BAS/src/pages/settings/UserSetting.jsx
+++ BAS/src/pages/settings/UserSetting.jsx
@@ -1,24 +1,22 @@
 "use client";
 
 import { Tabs } from "flowbite-react";
-import { HiAdjustments, HiClipboardList, HiUserCircle } from "react-icons/hi";
-import { MdDashboard } from "react-icons/md";
 import { MdAccountBox } from "react-icons/md";
 import { BsPersonVcardFill } from "react-icons/bs";
 
-import UpdateAccountDetails from "./updateAccountForm";
-import UpdatePersonalDetails from "./updatePersonalForm";
+import UpdateUserAccount from "./updateUserAccount";
+import UpdatePersonalAccount from "./updateUserPersonal";
 
-export default function Settings() {
+export default function UserSettings() {
   return (
     <div className="main-container">
       <h1 className="main-title">Update Setting</h1>
       <Tabs.Group aria-label="Tabs with icons" style="underline">
         <Tabs.Item active icon={MdAccountBox} title="Account Details">
-          <UpdateAccountDetails />
+          <UpdateUserAccount />
         </Tabs.Item>
         <Tabs.Item icon={BsPersonVcardFill} title="Personal Details">
-          <UpdatePersonalDetails />
+          <UpdatePersonalAccount />
         </Tabs.Item>
       </Tabs.Group>
     </div>
