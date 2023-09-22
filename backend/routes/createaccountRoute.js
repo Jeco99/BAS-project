7b89272e1c4e39254143b8643173aff8f2c85272
Jeco99 <jerriccojohncabunagan00@gmail.com>
2023-09-22 17:17:26 +0800

    working upload image on test

--- backend/routes/createaccountRoute.js
+++ backend/routes/createaccountRoute.js
@@ -2,8 +2,23 @@
 
 import  express  from "express";
 import sql from '../config/db.js';
+import multer from "multer";
+import path from 'path';
 
 const createAccountRouter = express.Router();
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'static/')
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+    cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname));
+  }
+})
+
+const upload = multer({ storage: storage })
+
 
 createAccountRouter.get("/", async (req, res) => {
     try {
@@ -14,32 +29,12 @@ createAccountRouter.get("/", async (req, res) => {
     }
   });
   
-  createAccountRouter.post("/create", async (req, res) => {
+  createAccountRouter.post("/create", 
+  upload.single('imagefile'),
+  async (req, res) => {
     try {
-      const {
-        user_image,
-        user_name,
-        email,
-        password,
-        first_name,
-        middle_name,
-        last_name,
-        suffix,
-        sex,
-        date_of_birth,
-        civil_status,
-        contact_number,
-        region,
-        province,
-        municipality,
-        barangay,
-        zone,
-        street,
-        zipcode,
-        user_type,
-      } = req.body;
-      // console.log(req.params);
-      const newUser = await sql`INSERT INTO user_details ( 
+      await sql`INSERT INTO user_details ( 
+      "user_type",
       "user_image", 
       "user_name",
       "email",
@@ -58,11 +53,31 @@ createAccountRouter.get("/", async (req, res) => {
       "barangay", 
       "zone", 
       "street", 
-      "zipcode", 
-      "user_type") VALUES(${user_image}, ${user_name}, ${email}, ${password}, ${first_name}, 
-        ${middle_name}, ${last_name}, ${suffix}, ${sex}, ${date_of_birth}, ${civil_status}, 
-        ${contact_number}, ${region},  ${province}, ${municipality}, ${barangay}, ${zone}, ${street}, ${zipcode}, ${user_type}) RETURNING *`;
-      res.json(newUser);
+      "zipcode"
+      ) VALUES(
+        ${req.body.user_type},
+        ${req.file.filename},
+        ${req.body.username},
+        ${req.body.email},
+        ${req.body.password},
+        ${req.body.confirmpassword},
+        ${req.body.firstname},
+        ${req.body.middlename},
+        ${req.body.lastname},
+        ${req.body.suffix},
+        ${req.body.dateofbirth},
+        ${req.body.contactnumber},
+        ${req.body.sex},
+        ${req.body.civilstatus},
+        ${req.body.region},
+        ${req.body.province},
+        ${req.body.municipal},
+        ${req.body.barangay},
+        ${req.body.zone},
+        ${req.body.street},
+        ${req.body.zipcode}
+      ) RETURNING *`;
+      // res.json(newUser);
       console.log(newUser);
     } catch (err) {
       console.error(err.message);
