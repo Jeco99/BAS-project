78b5347c5c04cff7f717ba9abdbd77e839795446
Jeco99 <jerriccojohncabunagan00@gmail.com>
2023-09-26 23:00:02 +0800

    fix post form, search and add post in dashboard

--- frontend/src/pages/user/userDashboard/userDashboard.jsx
+++ frontend/src/pages/user/userDashboard/userDashboard.jsx
@@ -1,3 +1,8 @@
+import { useState, useEffect } from "react";
+import BarangayAddPost from "./addpost";
+import { CgSearch } from "react-icons/cg";
+import { Input } from "@material-tailwind/react";
+
 import NumberOfFemales from "../../../components/chart/numberofFemale";
 import NumberOfMales from "../../../components/chart/numberofMale";
 import BarangayClearance from "../../../components/chart/numberofBarangayClearance";
@@ -5,14 +10,31 @@ import BarangayCertificate from "../../../components/chart/numberofBarangayCerti
 import BarangayPermit from "../../../components/chart/numberofBarangayPermit";
 
 import Post from "../../../components/post/post";
+import { useParams } from "react-router-dom";
+
+const userDetails_Selected_Loader = async (id) => {
+  const response = await fetch("http://localhost:3001/root/" + id);
+  const userDetails_data = await response.json();
+  return userDetails_data;
+};
 
 export default function UserDashboard() {
+  const [addPost, setAddPost] = useState(false);
+  const { id } = useParams();
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    async function init() {
+      const data = await userDetails_Selected_Loader(id);
+      setData(data);
+    }
+    init();
+  }, []);
+
+  console.log(data);
   return (
     <div className="main-container">
       <div>
-        <h1 className="main-title">
-          Dashboard
-        </h1>
+        <h1 className="main-title">Dashboard</h1>
         <div className="chartContainer">
           <NumberOfFemales />
           <NumberOfMales />
@@ -22,9 +44,37 @@ export default function UserDashboard() {
         </div>
       </div>
       <div>
-        <h1 className="main-title">
-          Latest News/Events
-        </h1>
+        <div>
+          <div className="flex justify-between items-center">
+            <h1 className="main-title">Latest News/Events</h1>
+            <div className="flex flex-col lg:flex-row">
+              <div className="relative">
+                <Input
+                  label="Type to search"
+                  // value={searchTerm}
+                  // onChange={handleSearch}
+                />
+                <div className="absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none">
+                  <CgSearch className="text-gray-400" />
+                </div>
+              </div>
+              {data.user_type == "admin" ? (
+                <button
+                  className="btn btnRadius"
+                  type="button"
+                  onClick={() => {
+                    setAddPost(true);
+                  }}
+                >
+                  Add Post
+                </button>
+              ) : (
+                <></>
+              )}
+            </div>
+          </div>
+        </div>
+        {addPost && <BarangayAddPost setAddPost={setAddPost} data={data} />}
         <div className="ml-4 text-2xl">
           <Post />
         </div>
